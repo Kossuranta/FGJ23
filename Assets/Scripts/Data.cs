f3d59eabcd140b35eaec0bccae3f8b084e16bf25
Kossuranta <kossuranta@gmail.com>
2023-02-04 00:15:57 +0200

    Set default values for SprintDuration, SprintSpeedMultiplier, JumpForce and Gravity

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -1,8 +1,8 @@
 public class Data
 {
     public float MoveSpeed { get; set; }
-    public float SprintDuration { get; set; }
-    public int SprintSpeedMultiplier { get; set; }
-    public float JumpForce { get; set; }
-    public float Gravity { get; set; }
+    public float SprintDuration { get; set; } = 3f;
+    public int SprintSpeedMultiplier { get; set; } = 2;
+    public float JumpForce { get; set; } = 5f;
+    public float Gravity { get; set; } = 9.81f;
 }
