1ce60959023639e949674d9917dc589a3f31e7f1
Tepitron <teppo.j.nieminen@tuni.fi>
2023-02-04 00:23:41 +0200

    Auto stash before merge of "main" and "origin/main"

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -1,6 +1,6 @@
 public class Data
 {
-    public float MoveSpeed { get; set; }
+    public float MoveSpeed { get; set; } = 10f;
     public float SprintDuration { get; set; } = 3f;
     public int SprintSpeedMultiplier { get; set; } = 2;
     public float JumpForce { get; set; } = 5f;
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,25 @@
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-{   
-    public float playerSpeed = 10;
+{      
+    [SerializeField]
+    private Rigidbody2D m_rigidbody;
+    private float m_moveSpeed;
+    private GameManager m_gameManager;
+
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        m_gameManager = GameManager.s_gameManager;
+        m_moveSpeed = m_gameManager.Data.MoveSpeed;
+       
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.Translate(Vector3.right * Time.deltaTime * playerSpeed);
+        m_rigidbody.MovePosition(transform.position * Time.deltaTime * m_moveSpeed);
     }
 
     public void SetPosition(Vector3 _pos)
