0f248decf072f8274f3499e44a10a4dd3b7fc804
Kossuranta <kossuranta@gmail.com>
2023-02-03 23:04:30 +0200

    Add values to Data classMake GameManager singleton

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -1,4 +1,8 @@
 public class Data
 {
-    
+    public float MoveSpeed { get; set; }
+    public float SprintDuration { get; set; }
+    public int SprintSpeedMultiplier { get; set; }
+    public float JumpForce { get; set; }
+    public float Gravity { get; set; }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager s_gameManager; // Illegal singleton
     public Data Data { get; private set; }
 
     private void Awake()
     {
+        s_gameManager = this;
+        
         Data = new Data();
     }
 }
