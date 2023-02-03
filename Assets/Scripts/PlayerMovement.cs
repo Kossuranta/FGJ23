ec5395a357aad34b6aca416b1adc2f5bb77ca751
Kossuranta <kossuranta@gmail.com>
2023-02-04 00:30:51 +0200

    Fix player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,20 +6,17 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D m_rigidbody;
     private float m_moveSpeed;
     private GameManager m_gameManager;
-
-
-    // Start is called before the first frame update
+    
     private void Start()
     {
         m_gameManager = GameManager.s_gameManager;
         m_moveSpeed = m_gameManager.Data.MoveSpeed;
        
     }
-
-    // Update is called once per frame
-    void FixedUpdate()
+    
+    private void FixedUpdate()
     {
-        m_rigidbody.MovePosition(transform.position * Time.deltaTime * m_moveSpeed);
+        m_rigidbody.MovePosition(transform.position + Vector3.right * (Time.deltaTime * m_moveSpeed));
     }
 
     public void SetPosition(Vector3 _pos)
