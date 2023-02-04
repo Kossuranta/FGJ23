0f5419c6d681c521d0c40e53fa126c201c151636
Kossuranta <kossuranta@gmail.com>
2023-02-04 12:36:09 +0200

    Remove obsolete code from BaseNode

--- Assets/Scripts/BaseNode.cs
+++ Assets/Scripts/BaseNode.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public abstract class BaseNode : MonoBehaviour
 {
     [SerializeField]
     private Transform m_playerPosition;
 
-    protected GameManager m_gameManager;
-    protected Data m_data;
-
     private void Start()
     {
-        m_gameManager = GameManager.Instance;
-        m_data = m_gameManager.Data;
         Initialize();
     }
 
