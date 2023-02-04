227cc466dfd7e46c11e789cd432b0790a192bd5d
Tepitron <teppo.j.nieminen@tuni.fi>
2023-02-04 19:34:54 +0200

    tutorial level

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -3,6 +3,6 @@ public class Data
     public float MoveSpeed { get; set; } = 10f;
     public float SprintDuration { get; set; } = 3f;
     public int SprintSpeedMultiplier { get; set; } = 2;
-    public float JumpForce { get; set; } = 8f;
+    public float JumpForce { get; set; } = 12f;
     public float Gravity { get; set; } = 9.81f;
 }
