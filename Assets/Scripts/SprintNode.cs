35b073e810dd1a4d70b9141be91cf2b1e5f0e82b
Kossuranta <kossuranta@gmail.com>
2023-02-04 13:16:58 +0200

    Remove obsolete code from SprintNode

--- Assets/Scripts/SprintNode.cs
+++ Assets/Scripts/SprintNode.cs
@@ -1,12 +1,9 @@
-﻿using UnityEngine;
-
-public class SprintNode : BaseNode
+﻿public class SprintNode : BaseNode
 {
     protected override void Initialize() { }
 
     protected override void Activate(PlayerMovement _playerMovement)
     {           
         _playerMovement.Sprint();
-        
     }
 }
