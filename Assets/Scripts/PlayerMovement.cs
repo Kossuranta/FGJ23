f601980d0bbb9f9155ace5c2f98e12c7974e455b
Kossuranta <kossuranta@gmail.com>
2023-02-04 02:44:38 +0200

    Improve players movement somewhat by using Vector2 instead of Vector3

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,19 +4,19 @@ public class PlayerMovement : MonoBehaviour
 {      
     [SerializeField]
     private Rigidbody2D m_rigidbody;
+    
     private float m_moveSpeed;
     private GameManager m_gameManager;
-    
+
     private void Start()
     {
-        m_gameManager = GameManager.s_gameManager;
+        m_gameManager = GameManager.Instance;
         m_moveSpeed = m_gameManager.Data.MoveSpeed;
-       
     }
     
     private void FixedUpdate()
     {
-        m_rigidbody.MovePosition(transform.position + Vector3.right * (Time.deltaTime * m_moveSpeed));
+        m_rigidbody.MovePosition((Vector2)transform.position + Vector2.right * (Time.deltaTime * m_moveSpeed));
     }
 
     public void SetPosition(Vector3 _pos)
