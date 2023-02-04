ae1390de306affbde7c2ffb111f93b77cc348c8c
Kossuranta <kossuranta@gmail.com>
2023-02-04 20:47:27 +0200

    Add IdleAnimator scriptMake altar animate

---
+++ Assets/Scripts/IdleAnimator.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class IdleAnimator : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer m_spriteRenderer;
+    
+    [SerializeField]
+    private Sprite[] m_sprites;
+    
+    [SerializeField]
+    private float m_animationDelay = 0.5f;
+    
+    private int m_animationIndex;
+    private float m_timer;
+
+    private void Awake()
+    {
+        if (m_spriteRenderer == null)
+        {
+            Debug.LogError("IdleAnimator is missing SpriteRenderer");
+            enabled = false;
+        }
+
+        if (m_sprites.Length == 0)
+        {
+            Debug.LogError("IdleAnimator is missing Sprites");
+            enabled = false;
+        }
+    }
+
+    private void Update()
+    {
+        m_timer += Time.deltaTime;
+        if (m_timer < m_animationDelay)
+            return;
+        
+        m_timer = 0;
+
+        if (m_animationIndex >= m_sprites.Length)
+            m_animationIndex = 0;
+        m_spriteRenderer.sprite = m_sprites[m_animationIndex];
+        m_animationIndex++;
+    }
+}
