13e02b5ecdf82b7138e6da66a316821846fae971
Kossuranta <kossuranta@gmail.com>
2023-02-05 05:29:17 +0200

    Show/Hide side slider

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -21,8 +23,22 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private SliderManager m_sliderManager;
 
+    [SerializeField]
+    private RectTransform m_sideSlider;
+
+    [SerializeField]
+    private int m_sideSliderHiddenPos;
+
+    [SerializeField]
+    private int m_sideSliderVisiblePos;
+
+    [SerializeField]
+    private float m_sideSliderAnimSpeed = 3f;
+
     private GameManager m_gameManager;
-    
+    private bool m_showSideSlider = true;
+    private float m_timer;
+
     public void Initialize(GameManager _gameManager)
     {
         m_gameManager = _gameManager;
@@ -36,9 +52,37 @@ public class UIManager : MonoBehaviour
         UpdateButtonStates();
 
         m_gameManager.a_runStart += UpdateButtonStates;
+        m_gameManager.a_runStart += HideSideSlider;
         m_gameManager.a_runReset += UpdateButtonStates;
+        m_gameManager.a_runReset += ShowSideSlider;
         
         m_levelCompletedButtons.SetActive(false);
+        
+        Vector2 pos = m_sideSlider.anchoredPosition;
+        pos.x = m_sideSliderHiddenPos;
+        m_sideSlider.anchoredPosition = pos;
+    }
+
+    private void Update()
+    {
+        if (m_showSideSlider)
+        {
+            if (m_timer >= 1f)
+                return;
+
+            m_timer += Time.deltaTime * m_sideSliderAnimSpeed;
+        }
+        else
+        {
+            if (m_timer <= 0f)
+                return;
+            
+            m_timer -= Time.deltaTime * m_sideSliderAnimSpeed;
+        }
+        
+        Vector2 pos = m_sideSlider.anchoredPosition;
+        pos.x = Mathf.Lerp(m_sideSliderHiddenPos, m_sideSliderVisiblePos, m_timer);
+        m_sideSlider.anchoredPosition = pos;
     }
 
     public void ShowLevelCompletedButtons()
@@ -67,5 +111,20 @@ public class UIManager : MonoBehaviour
     {
         m_btnStart.gameObject.SetActive(!m_gameManager.IsRunning);
         m_btnReset.gameObject.SetActive(m_gameManager.IsRunning);
+    }
+
+    private void HideSideSlider()
+    {
+        m_showSideSlider = false;
+    }
+
+    private void ShowSideSlider()
+    {
+        m_showSideSlider = true;
+    }
+
+    public void ToggleSideSlider()
+    {
+        m_showSideSlider = !m_showSideSlider;
     }
 }
